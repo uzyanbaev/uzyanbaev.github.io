677e7243cfa03f0089d88e7a5ffc0007f66835a0
Radmir <uzyanbaevradmir@gmail.com>
2023-09-06 16:06:38 +0500

    project ready

--- src/js/script.js
+++ src/js/script.js
@@ -15,3 +15,24 @@ const counters = document.querySelectorAll('.experience__scale-counter'),
 counters.forEach( (item, i) => {
 	lines[i].style.width = item.innerHTML;
 });
+
+
+function showTab(tabIndex) {
+	var contents = document.querySelectorAll('.content');
+	for (var i = 0; i < contents.length; i++) {
+		contents[i].style.display = 'none';
+	}
+
+	var tabs = document.querySelectorAll('.tab');
+	for (var i = 0; i < tabs.length; i++) {
+		tabs[i].classList.remove('active');
+	}
+
+	document.getElementById('content' + tabIndex).style.display = 'block';
+	document.getElementById('tab' + tabIndex).classList.add('active');
+}
+
+function hideTab(tabIndex) {
+	document.getElementById('content' + tabIndex).style.display = 'none';
+	document.getElementById('tab' + tabIndex).classList.remove('active');
+}
