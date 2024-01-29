2327b725fcbd9b5f4ede4ec1ef80b13de883aa4f
Radmir Uzyanbaev <uzyanbaevradmir@gmail.com>
2024-01-30 00:06:30 +0500

    added russian language, and some changes

--- src/js/script.js
+++ src/js/script.js
@@ -65,3 +65,23 @@ function copyEmail() {
     copyMessage.textContent = "email copied";
     setTimeout(function() { copyMessage.textContent = ""; }, 1500);
 }
+
+function changeLanguage(id) {
+    fetch('../data/translation.json')
+        .then(response => response.json())
+        .then(data => {
+            for (let key in data) {
+                let element = document.querySelector(`#${key}`);
+                if (element) {
+                    element.textContent = data[key][id];
+                }
+            }
+
+            document.body.className = id;
+        })
+        .catch(error => console.error('Error:', error));
+}
+
+function resetLanguage() {
+    location.reload();
+}
