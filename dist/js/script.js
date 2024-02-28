7872f310d36bb82fbed752d976d399fef130d378
Radmir <uzyanbaevradmir@gmail.com>
2024-02-29 03:40:14 +0500

    added project filter

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,62 +1,22 @@
-const hamburger = document.querySelector('.hamburger');
-const menu = document.querySelector('.menu');
-const closeElem = document.querySelector('.menu__close');
-const menuLinks = document.querySelectorAll('.menu__link a, .menu__link button');
+const filterDropdown = document.getElementById('filter-dropdown');
+const projects = document.querySelectorAll('.project');
+const copyMessage = document.getElementById('copy-message');
 
-hamburger.addEventListener('click', toggleMenu);
-closeElem.addEventListener('click', toggleMenu);
-
-menuLinks.forEach(link => {
-  link.addEventListener('click', () => {
-    if (menu.classList.contains('active')) {
-      toggleMenu();
-    }
-  });
+document.addEventListener('DOMContentLoaded', () => {
+    filterDropdown.addEventListener('change', filterProjects);
 });
 
-function toggleMenu() {
-  menu.classList.toggle('active');
-  document.body.classList.toggle('menu-open');
+function filterProjects() {
+    const filterValue = filterDropdown.value;
+
+    projects.forEach(project => {
+        project.style.display = 'none';
+
+        if (filterValue === 'all' || project.classList.contains(filterValue)) {
+            project.style.display = 'block';
+        }
+    });
 }
-
-function copyEmail() {
-    var email = document.querySelector('.main__email-text a').textContent;
-    var tempInput = document.createElement("input");
-    document.body.appendChild(tempInput);
-    tempInput.value = email;
-    tempInput.select();
-    document.execCommand("copy");
-    document.body.removeChild(tempInput);
-
-    var copyMessage = document.getElementById("copy-message");
-    copyMessage.textContent = "email copied";
-    setTimeout(function() { copyMessage.textContent = ""; }, 1500);
-}
-
-function changeLanguage(id) {
-    fetch('../data/translation.json')
-        .then(response => response.json())
-        .then(data => {
-            document.body.className = id;
-            document.documentElement.lang = id;
-
-            document.title = data['title'][id];
-            const metaDescription = document.querySelector('meta[name="description"]');
-            if (metaDescription) {
-                metaDescription.content = data['description'][id];
-            }
-
-            for (let key in data) {
-                let element = document.querySelector(`#${key}`);
-                if (element) {
-                    element.innerHTML = data[key][id];
-                }
-            }
-        })
-        .catch(error => console.error('Error:', error));
-}
-
-
 
 function openPage(pageId, initial = false) {
     var i, tabcontent, tablinks;
@@ -83,11 +43,45 @@ function openPage(pageId, initial = false) {
         window.scrollTo(0, 0);
     }, 300);
 }
+function copyEmail() {
+    const email = document.querySelector('.main__email-text a').textContent;
+    const tempInput = document.createElement('input');
+    document.body.appendChild(tempInput);
+    tempInput.value = email;
+    tempInput.select();
+    document.execCommand('copy');
+    document.body.removeChild(tempInput);
 
-document.addEventListener("DOMContentLoaded", function() {
-    openPage('main', true);
-});
+    copyMessage.textContent = 'Email copied';
+    setTimeout(() => {
+        copyMessage.textContent = '';
+    }, 1500);
+}
 
+function changeLanguage(id) {
+    fetch('../data/translation.json')
+        .then(response => response.json())
+        .then(data => {
+            document.body.className = id;
+            document.documentElement.lang = id;
+
+            document.title = data.title[id];
+            const metaDescription = document.querySelector('meta[name="description"]');
+            
+            if (metaDescription) {
+                metaDescription.content = data.description[id];
+            }
+
+            for (let key in data) {
+                const element = document.querySelector(`#${key}`);
+                
+                if (element) {
+                    element.innerHTML = data[key][id];
+                }
+            }
+        })
+        .catch(error => console.error('Error:', error));
+}
 
 function updateMetaTags(pageId) {
     var title, description;
